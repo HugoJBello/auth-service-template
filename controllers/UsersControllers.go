d9080783c80080b6e1afe28ee74960fe0a922e19
hjbello <hjbello.wk@gmail.com>
2019-08-19 18:08:40 +0200

    admin middleware

--- controllers/UsersControllers.go
+++ controllers/UsersControllers.go
@@ -1,20 +1,24 @@
 package controllers
 
 import (
+	"auth-service-template/models"
 	u "auth-service-template/utils"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/context"
 )
 
 var ListUsers = func(w http.ResponseWriter, r *http.Request) {
-	limit := r.FormValue("limit")
-	skip := r.FormValue("skip")
-	val := context.Get(r, "user")
+	limit, _ := strconv.Atoi(r.FormValue("limit"))
+	skip, _ := strconv.Atoi(r.FormValue("skip"))
+	user := context.Get(r, "user")
 
+	users := models.GetUsers(limit, skip)
 	fmt.Println(limit)
 	fmt.Println(skip)
-	fmt.Println(val)
+	fmt.Println(users)
+	fmt.Println(user)
 	u.Respond(w, nil)
 }
